7218e902f1c7bd70c6b3d45c4f9efe717310419a
Jackson Harrison <Jackson.Harrison@stu.barren.kyschools.us>
2024-03-04 08:00:22 -0600

    Initial Commit

---
+++ iExpense”/iExpense_App.swift
@@ -0,0 +1,17 @@
+//
+//  iExpense_App.swift
+//  iExpense”
+//
+//  Created by Jackson Harrison on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpense_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
