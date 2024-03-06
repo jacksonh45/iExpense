744c43674e45ade44feae56abd452b9a6f720308
Jackson Harrison <Jackson.Harrison@stu.barren.kyschools.us>
2024-03-06 10:46:28 -0600

    Actaully completed project

--- iExpense”/ContentView.swift
+++ iExpense”/ContentView.swift
@@ -7,18 +7,87 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+struct ExpenseItem: Identifiable, Codable {
+    var id = UUID()
+    let name: String
+    let type: String
+    let amount: Double
 }
 
-#Preview {
-    ContentView()
+class Expenses: ObservableObject {
+    @Published var items = [ExpenseItem]() {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+    
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "Items") {
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
+                items = decodedItems
+                return
+            }
+        }
+        items = []
+    }
+    
+    
+}
+
+struct ContentView: View {
+    @StateObject var expenses = Expenses()
+    @State private var showingAddExpense = false
+    
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(expenses.items) {item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type)
+                        }
+                        
+                        Spacer()
+                        Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                            .foregroundColor(self.expenseStyles(forAmount: Int(item.amount)))
+                    }
+                }.onDelete(perform: removeItems)
+            } .toolbar {
+                Button {
+                    showingAddExpense = true
+                } label: {
+                    Image(systemName: "plus")
+                }
+            }
+            .navigationTitle("iExpense")
+        }
+        .sheet(isPresented: $showingAddExpense) {
+            AddView(expenses: expenses)
+        }
+    }
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
+    }
+    func expenseStyles(forAmount amount: Int) -> Color {
+        switch amount {
+        case Int.min..<11:
+            return .green
+        case 11..<100:
+            return .yellow
+        default:
+            return .red
+        }
+    }
+   
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
--- iExpense”/iExpense_App.swift
+++ iExpense”/iExpense_App.swift
@@ -4,11 +4,10 @@
 //
 //  Created by Jackson Harrison on 3/4/24.
 //
-
 import SwiftUI
 
 @main
-struct iExpense_App: App {
+struct iExpenseApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
